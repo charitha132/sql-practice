557ffb874683204e73f31b6026bc886c8a1f8bcf
charitha132 <papireddycharitha@gmail.com>
2026-04-02 16:38:49 +0530

    Update hackerank.sql

--- hackerank.sql
+++ hackerank.sql
@@ -51,6 +51,50 @@ SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
 
 
+-- Problem 10
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE 'A%'
+OR CITY LIKE 'E%'
+OR CITY LIKE 'I%'
+OR CITY LIKE 'O%'
+OR CITY LIKE 'U%';
+
+
+-- Problem 11
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1)IN ("A","E","I","O","U");
+
+
+-- Problem 12
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE LEFT(CITY,1) IN ("A","E","I","O","U") 
+AND RIGHT(CITY,1) IN ("A","E","I","O","U"); 
+
+
+-- Problem 13
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION
+WHERE NOT LEFT(CITY,1) IN ("A","E","I","O","U");
+
+
+-- Problem 14
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) NOT IN ("A","E","I","O","U");
+
+
+-- Problem 15
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') OR RIGHT(CITY,1) NOT IN ('A','E','I','O','U');
+
+
+
 
 
 
